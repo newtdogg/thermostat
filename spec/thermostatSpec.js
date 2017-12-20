94c517b0b39a6258f2e6c6b216e92fabde44c196
newtdogg <edwardfrancisnewton@live.com>
2017-12-20 15:36:57 +0000

    added reset and power saving tests

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -6,32 +6,44 @@ describe("Thermostat", function() {
   });
 
   describe("Start up", function() {
-    it ("should start at 20°C celsius", function() {
+    it ("Should start at 20°C celsius", function() {
       expect(thermostat.currentTemperature).toEqual(20)
     });
   });
 
-  describe("adjusting temperature", function() {
-    it ("temperature can be increased", function() {
-      thermostat.increaseTemp()
+  describe("Adjusting temperature", function() {
+    it ("Temperature can be increased", function() {
+      thermostat.increaseTemp();
       expect(thermostat.currentTemperature).toEqual(21)
     });
 
-    it ("temperature can be decreased", function() {
-      thermostat.decreaseTemp()
+    it ("Temperature can be decreased", function() {
+      thermostat.decreaseTemp();
       expect(thermostat.currentTemperature).toEqual(19)
     });
+    it ("Temperature can be reset to starting temperature", function() {
+      thermostat.decreaseTemp();
+      thermostat.resetTemp();
+      expect(thermostat.currentTemperature).toEqual(20)
+    });
+
   });
 
-  describe("temperature limits", function() {
-    it ("temperature cannot be decreased below 10°C", function() {
+  describe("Temperature limits", function() {
+    it ("Temperature cannot be decreased below 10°C", function() {
       for (var i=1; i <= 10; i++) thermostat.decreaseTemp();
       expect(function() { thermostat.decreaseTemp() }).toThrowError("Minimum temperature is 10°C")
     });
 
-    // it ("temperature can be decreased", function() {
-    //   thermostat.decreaseTemp()
-    //   expect(thermostat.currentTemperature).toEqual(19)
-    // });
+    it ("Temperature cannot be increased above 32°C", function() {
+      for (var i=1; i <= 12; i++) thermostat.increaseTemp();
+      expect(function() { thermostat.increaseTemp() }).toThrowError("Maximum temperature is 32°C")
+    });
+
+    it ("Temperature cannot be increased above 25°C when power saving mode is on", function() {
+      for (var i=1; i <= 5; i++) thermostat.increaseTemp();
+      expect(function() { thermostat.increaseTemp() }).toThrowError("Maximum temperature is 25°C with power saving mode on")
+    });
+
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,16 +1,25 @@
 const STARTING_TEMPERATURE = 20;
+const MIN_TEMP = 10;
+const MAX_TEMP = 32;
 
 function Thermostat(){
   this.currentTemperature = STARTING_TEMPERATURE;
 };
 
 Thermostat.prototype.increaseTemp = function () {
+  if(this.currentTemperature >= MAX_TEMP){
+    throw new Error("Maximum temperature is 32°C");
+  }
   this.currentTemperature++
 };
 
 Thermostat.prototype.decreaseTemp = function () {
-  if(this.currentTemperature <= 10){
+  if(this.currentTemperature <= MIN_TEMP){
     throw new Error("Minimum temperature is 10°C");
   }
   this.currentTemperature--
+};
+
+Thermostat.prototype.resetTemp = function () {
+  this.currentTemperature = STARTING_TEMPERATURE;
 };
